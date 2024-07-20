a6fe99994fe05e6ef40e61a73a18c5f167c1a1a1
José Pablo Gómez <jgomez@Joses-MacBook-Pro.local>
2024-07-20 15:04:06 -0600

    Inicial Commit: React Front End structure creation.

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
